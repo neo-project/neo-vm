7c02fd6976c71187ffb3bd557c4b150a68561c74
Erik Zhang <erik@vcage.com>
2016-12-27 19:42:33 +0800

    improve efficiency

--- src/AntShares.VM/ApiService.cs
+++ src/AntShares.VM/ApiService.cs
@@ -1,43 +1,52 @@
-﻿namespace AntShares.VM
+﻿using System;
+using System.Collections.Generic;
+
+namespace AntShares.VM
 {
     public class ApiService
     {
-        public virtual bool Invoke(string method, ScriptEngine engine)
+        private Dictionary<string, Func<ScriptEngine, bool>> dictionary = new Dictionary<string, Func<ScriptEngine, bool>>();
+
+        public ApiService()
         {
-            switch (method)
-            {
-                case "System.ScriptEngine.GetScriptContainer":
-                    return GetScriptContainer(engine);
-                case "System.ScriptEngine.GetExecutingScriptHash":
-                    return GetExecutingScriptHash(engine);
-                case "System.ScriptEngine.GetCallingScriptHash":
-                    return GetCallingScriptHash(engine);
-                case "System.ScriptEngine.GetEntryScriptHash":
-                    return GetEntryScriptHash(engine);
-                default:
-                    return false;
-            }
+            Register("System.ScriptEngine.GetScriptContainer", GetScriptContainer);
+            Register("System.ScriptEngine.GetExecutingScriptHash", GetExecutingScriptHash);
+            Register("System.ScriptEngine.GetCallingScriptHash", GetCallingScriptHash);
+            Register("System.ScriptEngine.GetEntryScriptHash", GetEntryScriptHash);
         }
 
-        private bool GetScriptContainer(ScriptEngine engine)
+        protected bool Register(string method, Func<ScriptEngine, bool> handler)
+        {
+            if (dictionary.ContainsKey(method)) return false;
+            dictionary.Add(method, handler);
+            return true;
+        }
+
+        internal bool Invoke(string method, ScriptEngine engine)
+        {
+            if (!dictionary.ContainsKey(method)) return false;
+            return dictionary[method](engine);
+        }
+
+        private static bool GetScriptContainer(ScriptEngine engine)
         {
             engine.EvaluationStack.Push(new StackItem(engine.ScriptContainer));
             return true;
         }
 
-        private bool GetExecutingScriptHash(ScriptEngine engine)
+        private static bool GetExecutingScriptHash(ScriptEngine engine)
         {
             engine.EvaluationStack.Push(engine.Crypto.Hash160(engine.ExecutingScript));
             return true;
         }
 
-        private bool GetCallingScriptHash(ScriptEngine engine)
+        private static bool GetCallingScriptHash(ScriptEngine engine)
         {
             engine.EvaluationStack.Push(engine.Crypto.Hash160(engine.CallingScript));
             return true;
         }
 
-        private bool GetEntryScriptHash(ScriptEngine engine)
+        private static bool GetEntryScriptHash(ScriptEngine engine)
         {
             engine.EvaluationStack.Push(engine.Crypto.Hash160(engine.EntryScript));
             return true;
