614428151819d2b3d1d5fe4c0006fd5ceff22cb3
Erik Zhang <erik@vcage.com>
2017-04-17 20:49:23 +0800

    add Main method to AntShares.Compiler.MSIL

--- src/AntShares.Compiler.MSIL/Program.cs
+++ src/AntShares.Compiler.MSIL/Program.cs
@@ -1,7 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
+﻿using System.IO;
 
 namespace AntShares.Compiler.MSIL
 {
@@ -9,6 +6,15 @@ namespace AntShares.Compiler.MSIL
     {
         public static void Main(string[] args)
         {
+            if (args.Length == 0) return;
+            if (!File.Exists(args[0])) return;
+            byte[] script;
+            using (FileStream fs = new FileStream(args[0], FileMode.Open, FileAccess.Read))
+            {
+                script = Converter.Convert(fs);
+            }
+            string out_path = args.Length >= 2 ? args[1] : Path.ChangeExtension(args[0], "avm");
+            File.WriteAllBytes(out_path, script);
         }
     }
 }
