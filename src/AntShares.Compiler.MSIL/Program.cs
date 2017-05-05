cf92075406c9dd55997937243d8f8043a1d9b623
lights li <lightsever@hotmail.com>
2017-05-05 23:08:43 +0800

    fix

--- src/AntShares.Compiler.MSIL/Program.cs
+++ src/AntShares.Compiler.MSIL/Program.cs
@@ -7,8 +7,18 @@ namespace AntShares.Compiler.MSIL
 {
     public class Program
     {
+        //Console.WriteLine("helo ha:"+args[0]); //普通输出
+        //Console.WriteLine("<WARN> 这是一个严重的问题。");//警告输出，黄字
+        //Console.WriteLine("<WARN|aaaa.cs(1)> 这是ee一个严重的问题。");//警告输出，带文件名行号
+        //Console.WriteLine("<ERR> 这是一个严重的问题。");//错误输出，红字
+        //Console.WriteLine("<ERR|aaaa.cs> 这是ee一个严重的问题。");//错误输出，带文件名
+        //Console.WriteLine("SUCC");//输出这个表示编译成功
+        //控制台输出约定了特别的语法
         public static void Main(string[] args)
         {
+
+            Console.OutputEncoding = System.Text.Encoding.UTF8;
+
         }
     }
 }
