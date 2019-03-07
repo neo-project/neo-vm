36ad157f31c29ab54b3bd0ac20b5b9a5bcd241ed
Shargon <shargon@gmail.com>
2019-03-07 10:10:40 +0100

    Implement for tests

--- tests/neo-vm.Tests/Types/ManualScriptTable.cs
+++ tests/neo-vm.Tests/Types/ManualScriptTable.cs
@@ -1,4 +1,6 @@
-﻿using System.Collections.Generic;
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
 using Neo.Test.Extensions;
 using Neo.VM;
 
@@ -6,6 +8,22 @@ namespace Neo.Test.Types
 {
     public class ManualScriptTable : IScriptTable
     {
+        class ByteArrayComparable : IEqualityComparer<byte[]>
+        {
+            public bool Equals(byte[] x, byte[] y)
+            {
+                return x.SequenceEqual(y);
+            }
+
+            public int GetHashCode(byte[] obj)
+            {
+                if (obj.Length < 4) return 0;
+
+                return BitConverter.ToInt32(obj);
+            }
+        }
+
+        private Dictionary<byte[], int> _scriptCounter = new Dictionary<byte[], int>(new ByteArrayComparable());
         private Dictionary<string, byte[]> _data = new Dictionary<string, byte[]>();
 
         public byte[] GetScript(byte[] script_hash)
@@ -21,6 +39,21 @@ namespace Neo.Test.Types
         public void Add(byte[] script)
         {
             _data.Add(Crypto.Default.Hash160(script).ToHexString(), script);
+        }
+
+        public void IncrementInvocationCounter(byte[] script_hash)
+        {
+            _scriptCounter[script_hash]++;
+        }
+
+        public int GetInvocationCounter(byte[] script_hash)
+        {
+            if (!_scriptCounter.TryGetValue(script_hash, out var value))
+            {
+                return 0;
+            }
+
+            return value;
         }
     }
 }
