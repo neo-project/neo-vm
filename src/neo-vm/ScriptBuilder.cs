5c607962d21d7ef6881ef76274fdbafc98f4aa64
erikzhang <erik@neo.org>
2019-04-08 21:18:41 +0800

    optimize

--- src/neo-vm/ScriptBuilder.cs
+++ src/neo-vm/ScriptBuilder.cs
@@ -1,6 +1,5 @@
 ﻿using System;
 using System.IO;
-using System.Linq;
 using System.Numerics;
 using System.Security.Cryptography;
 using System.Text;
@@ -97,25 +96,26 @@ namespace Neo.VM
 
         public ScriptBuilder EmitSysCall(string api, bool compress = true)
         {
-            if (api == null) throw new ArgumentNullException();
+            if (api == null) throw new ArgumentNullException(nameof(api));
+            if (api.Length == 0) throw new ArgumentException(nameof(api));
 
             byte[] api_bytes = Encoding.ASCII.GetBytes(api);
-            
-            if (api_bytes.Length == 0 || api_bytes.Length > 252)
-                throw new ArgumentException();
 
             if (compress)
             {
                 using (var sha = SHA256.Create())
-                {
                     api_bytes = sha.ComputeHash(api_bytes);
-                    Array.Resize(ref api_bytes, 4);
-                }
+                Array.Resize(ref api_bytes, 4);
+            }
+            else
+            {
+                if (api_bytes.Length > 252)
+                    throw new ArgumentException(nameof(api));
             }
 
             byte[] arg = new byte[api_bytes.Length + 1];
             arg[0] = (byte)api_bytes.Length;
-            Buffer.BlockCopy(api_bytes, 0, arg, 1, api_bytes.Length);
+            Unsafe.MemoryCopy(api_bytes, 0, arg, 1, api_bytes.Length);
             return Emit(OpCode.SYSCALL, arg);
         }
 
