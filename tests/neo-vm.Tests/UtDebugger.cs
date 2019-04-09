6b31afc6c36531d2e4f348c4b15130ccc4753fd9
Shargon <shargon@gmail.com>
2019-04-09 09:23:29 +0200

    Debugger unit testing (#115)

---
+++ tests/neo-vm.Tests/UtDebugger.cs
@@ -0,0 +1,39 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Neo.Test.Types;
+using Neo.VM;
+
+namespace Neo.Test
+{
+    [TestClass]
+    public class UtDebugger
+    {
+        [TestMethod]
+        public void TestBreakPoint()
+        {
+            using (var engine = new ExecutionEngine(null, Crypto.Default, null, null))
+            using (var script = new ScriptBuilder())
+            {
+                script.Emit(OpCode.NOP);
+                script.Emit(OpCode.NOP);
+                script.Emit(OpCode.NOP);
+                script.Emit(OpCode.NOP);
+
+                engine.LoadScript(script.ToArray());
+
+                var debugger = new Debugger(engine);
+
+                debugger.AddBreakPoint(engine.CurrentContext.ScriptHash, 3);
+                debugger.AddBreakPoint(engine.CurrentContext.ScriptHash, 4);
+                debugger.Execute();
+
+                Assert.AreEqual(3, engine.CurrentContext.InstructionPointer);
+                Assert.AreEqual(VMState.BREAK, engine.State);
+
+                debugger.RemoveBreakPoint(engine.CurrentContext.ScriptHash, 4);
+                debugger.Execute();
+
+                Assert.AreEqual(VMState.HALT, engine.State);
+            }
+        }
+    }
+}
