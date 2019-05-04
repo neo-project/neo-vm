d008a80171d9ffaf5f5616f1016072acd8a49d28
Shargon <shargon@gmail.com>
2019-05-04 09:58:46 +0200

    Remove dependency

--- tests/neo-vm.Tests/UtScript.cs
+++ tests/neo-vm.Tests/UtScript.cs
@@ -1,4 +1,3 @@
-using FluentAssertions;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
 using Neo.Test.Types;
 using Neo.VM;
@@ -20,7 +19,7 @@ namespace Neo.Test
         public void TestScriptHash()
         {
             byte[] b = new byte[] { 0x9F, 0x7F, 0xD0, 0x96, 0xD3, 0x7E, 0xD2, 0xC0, 0xE3, 0xF7, 0xF0, 0xCF, 0xC9, 0x24, 0xBE, 0xEF, 0x4F, 0xFC, 0xEB, 0x68 };
-            uut.ScriptHash.Should().BeEquivalentTo(b);
+            CollectionAssert.AreEqual(uut.ScriptHash, b);
         }
     }
 }
