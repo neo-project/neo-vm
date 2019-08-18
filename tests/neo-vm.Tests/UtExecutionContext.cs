73649cdc28a8395737b3d5f2f61ed715c28c4d7a
Shargon <shargon@gmail.com>
2019-08-18 20:20:14 +0200

    Add TryGetState (#194)

---
+++ tests/neo-vm.Tests/UtExecutionContext.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Neo.VM;
+
+namespace Neo.Test
+{
+    [TestClass]
+    public class UtExecutionContext
+    {
+        [TestMethod]
+        public void StateTest()
+        {
+            var context = new ExecutionContext(null, null, 0);
+
+            Assert.IsFalse(context.TryGetState<int>(out var i));
+            context.SetState(5);
+            Assert.AreEqual(5, context.GetState<int>());
+            Assert.IsTrue(context.TryGetState(out i));
+            Assert.AreEqual(5, i);
+        }
+    }
+}
