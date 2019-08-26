d49efbaa06be56b5f0556d5ed9e58736f6a1fc81
Vitor <vncoelho@gmail.com>
2019-08-26 15:26:24 -0300

    Manually fixing again

--- tests/neo-vm.Tests/UtDebugger.cs
+++ tests/neo-vm.Tests/UtDebugger.cs
@@ -69,11 +69,16 @@ namespace Neo.Test
             using (var engine = new ExecutionEngine())
             using (var script = new ScriptBuilder())
             {
-                /* ┌     */ script.EmitJump(OpCode.CALL, 5);
-                /* │  ┌> */ script.Emit(OpCode.NOT);
-                /* │  │  */ script.Emit(OpCode.RET);
-                /* └> │  */ script.Emit(OpCode.PUSH0);
-                /*  └─┘  */ script.Emit(OpCode.RET);
+                /* ┌     CALL 
+                   │  ┌> NOT
+                   │  │  RET
+                   └> │  PUSH0  
+                    └─┘  RET */ 
+                script.EmitJump(OpCode.CALL, 5);
+                script.Emit(OpCode.NOT);
+                script.Emit(OpCode.RET);
+                script.Emit(OpCode.PUSH0);
+                script.Emit(OpCode.RET);
 
                 engine.LoadScript(script.ToArray());
 
@@ -107,15 +112,15 @@ namespace Neo.Test
             using (var engine = new ExecutionEngine())
             using (var script = new ScriptBuilder())
             {
-                /* ┌     */
+                /* ┌     CALL
+                   │  ┌> NOT 
+                   │  │  RET
+                   └> │  PUSH0
+                    └─┘  RET */
                 script.EmitJump(OpCode.CALL, 5);
-                /* │  ┌> */
                 script.Emit(OpCode.NOT);
-                /* │  │  */
                 script.Emit(OpCode.RET);
-                /* └> │  */
                 script.Emit(OpCode.PUSH0);
-                /*  └─┘  */
                 script.Emit(OpCode.RET);
 
                 engine.LoadScript(script.ToArray());
@@ -163,11 +168,16 @@ namespace Neo.Test
             using (var engine = new ExecutionEngine())
             using (var script = new ScriptBuilder())
             {
-                /* ┌     */ script.EmitJump(OpCode.CALL, 5);
-                /* │  ┌> */ script.Emit(OpCode.NOT);
-                /* │  │  */ script.Emit(OpCode.RET);
-                /* └>X│  */ script.Emit(OpCode.PUSH0);
-                /*   └┘  */ script.Emit(OpCode.RET);
+                /* ┌     */
+                script.EmitJump(OpCode.CALL, 5);
+                /* │  ┌> */
+                script.Emit(OpCode.NOT);
+                /* │  │  */
+                script.Emit(OpCode.RET);
+                /* └>X│  */
+                script.Emit(OpCode.PUSH0);
+                /*   └┘  */
+                script.Emit(OpCode.RET);
 
                 engine.LoadScript(script.ToArray());
 
