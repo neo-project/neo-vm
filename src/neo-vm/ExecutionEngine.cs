912bbfe20d29a76ddbe24b897bc29d5025446ad9
Shargon <shargon@gmail.com>
2019-09-19 16:29:33 +0200

    Check items after copy an array/struct

--- src/neo-vm/ExecutionEngine.cs
+++ src/neo-vm/ExecutionEngine.cs
@@ -930,6 +930,7 @@ namespace Neo.VM
                                         : new Struct(array);
 
                                 context.EvaluationStack.Push(result);
+                                if (!CheckStackSize(false, int.MaxValue)) return false;
                             }
                             else
                             {
@@ -949,7 +950,6 @@ namespace Neo.VM
                                     : new Struct(items);
 
                                 context.EvaluationStack.Push(result);
-
                                 if (!CheckStackSize(true, count)) return false;
                             }
                             break;
