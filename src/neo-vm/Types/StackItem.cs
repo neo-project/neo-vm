985a961a6c6270d0c93601a7cafe75fdc9c99ec6
erikzhang <erik@neo.org>
2019-11-17 20:18:09 +0800

    Optimize `StackItem.Equals()`

--- src/neo-vm/Types/StackItem.cs
+++ src/neo-vm/Types/StackItem.cs
@@ -15,8 +15,7 @@ namespace Neo.VM.Types
 
         public sealed override bool Equals(object obj)
         {
-            if (obj == null) return false;
-            if (obj == this) return true;
+            if (ReferenceEquals(obj, this)) return true;
             if (obj is StackItem other)
                 return Equals(other);
             return false;
