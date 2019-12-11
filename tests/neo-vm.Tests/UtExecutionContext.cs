d6851ae0450df3a4234a9a1061178c53f811fb09
Shargon <shargon@gmail.com>
2019-12-11 13:57:55 +0100

    Test clone

--- tests/neo-vm.Tests/UtExecutionContext.cs
+++ tests/neo-vm.Tests/UtExecutionContext.cs
@@ -17,6 +17,21 @@ namespace Neo.Test
             stack.Push(100);
             stack = context.GetState<Stack<int>>();
             Assert.AreEqual(100, stack.Pop());
+            stack.Push(100);
+
+            // Test clone
+
+            var copy = context.Clone();
+            var copyStack = context.GetState<Stack<int>>();
+            Assert.AreEqual(1, copyStack.Count);
+            copyStack.Push(200);
+            copyStack = context.GetState<Stack<int>>();
+            Assert.AreEqual(200, copyStack.Pop());
+            Assert.AreEqual(100, copyStack.Pop());
+            copyStack.Push(200);
+
+            stack = context.GetState<Stack<int>>();
+            Assert.AreEqual(200, stack.Pop());
         }
     }
 }
