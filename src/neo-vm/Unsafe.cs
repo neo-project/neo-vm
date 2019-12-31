6c836e423d7f82a2f364b2e0dad98785ea3c256c
erikzhang <erik@neo.org>
2019-12-31 11:35:38 +0800

    Cleanup Unsafe.cs

--- src/neo-vm/Unsafe.cs
+++ src/neo-vm/Unsafe.cs
@@ -27,34 +27,5 @@ namespace Neo.VM
             }
             return false;
         }
-
-        [MethodImpl(MethodImplOptions.AggressiveInlining)]
-        public static bool MemoryEquals(byte[] x, byte[] y)
-        {
-            if (ReferenceEquals(x, y)) return true;
-            if (x is null || y is null) return false;
-            if (x == y) return true;
-            int len = x.Length;
-            if (len != y.Length) return false;
-            if (len == 0) return true;
-            fixed (byte* xp = x, yp = y)
-            {
-                long* xlp = (long*)xp, ylp = (long*)yp;
-                for (; len >= 8; len -= 8)
-                {
-                    if (*xlp != *ylp) return false;
-                    xlp++;
-                    ylp++;
-                }
-                byte* xbp = (byte*)xlp, ybp = (byte*)ylp;
-                for (; len > 0; len--)
-                {
-                    if (*xbp != *ybp) return false;
-                    xbp++;
-                    ybp++;
-                }
-            }
-            return true;
-        }
     }
 }
