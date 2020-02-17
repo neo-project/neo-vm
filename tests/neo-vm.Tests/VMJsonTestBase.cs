90242128940f24c16fd8df0148bac9388ba717b6
Luchuan <luchuan@neo.org>
2020-02-17 18:04:11 +0800

    reset VMjsonTestBase, UtVMJson

--- tests/neo-vm.Tests/VMJsonTestBase.cs
+++ tests/neo-vm.Tests/VMJsonTestBase.cs
@@ -20,49 +20,41 @@ namespace Neo.Test
         {
             foreach (var test in ut.Tests)
             {
-                Console.WriteLine("do test:" + test.Name);
-                try
+                using (var engine = new TestEngine())
                 {
-                    using (var engine = new TestEngine())
+                    Debugger debugger = new Debugger(engine);
+
+                    if (test.Script.Length > 0)
                     {
-                        Debugger debugger = new Debugger(engine);
+                        engine.LoadScript(test.Script);
+                    }
 
-                        if (test.Script.Length > 0)
+                    // Execute Steps
+
+                    if (test.Steps != null)
+                    {
+                        foreach (var step in test.Steps)
                         {
-                            engine.LoadScript(test.Script);
-                        }
+                            // Actions
 
-                        // Execute Steps
-
-                        if (test.Steps != null)
-                        {
-                            foreach (var step in test.Steps)
-                            {
-                                // Actions
-
-                                if (step.Actions != null) foreach (var run in step.Actions)
+                            if (step.Actions != null) foreach (var run in step.Actions)
+                                {
+                                    switch (run)
                                     {
-                                        switch (run)
-                                        {
-                                            case VMUTActionType.Execute: debugger.Execute(); break;
-                                            case VMUTActionType.StepInto: debugger.StepInto(); break;
-                                            case VMUTActionType.StepOut: debugger.StepOut(); break;
-                                            case VMUTActionType.StepOver: debugger.StepOver(); break;
-                                        }
+                                        case VMUTActionType.Execute: debugger.Execute(); break;
+                                        case VMUTActionType.StepInto: debugger.StepInto(); break;
+                                        case VMUTActionType.StepOut: debugger.StepOut(); break;
+                                        case VMUTActionType.StepOver: debugger.StepOver(); break;
                                     }
+                                }
 
-                                // Review results
+                            // Review results
 
-                                var add = string.IsNullOrEmpty(step.Name) ? "" : "-" + step.Name;
+                            var add = string.IsNullOrEmpty(step.Name) ? "" : "-" + step.Name;
 
-                                AssertResult(step.Result, engine, $"{ut.Category}-{ut.Name}-{test.Name}{add}: ");
-                            }
+                            AssertResult(step.Result, engine, $"{ut.Category}-{ut.Name}-{test.Name}{add}: ");
                         }
                     }
-                }
-                catch (Exception err)
-                {
-                    throw new Exception(test.Name + " error", err);
                 }
             }
         }
