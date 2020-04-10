aa75eb770185579f175764bf88dc4cbbae7397ad
erikzhang <erik@neo.org>
2020-04-10 15:57:40 +0800

    Update ExceptionHandingContext.cs

--- src/neo-vm/ExceptionHandingContext.cs
+++ src/neo-vm/ExceptionHandingContext.cs
@@ -6,20 +6,20 @@ namespace Neo.VM
     public sealed class ExceptionHandingContext
     {
         public ExecutionContext ExecutionContext { get; private set; }
-        public int CatchPointer { get; private set; }
-        public int FinallyPointer { get; private set; }
-        public int EndPointer { get; internal set; }
-        public bool HasCatch { get; private set; }
-        public bool HasFinally { get; private set; }
+        public int CatchPointer { get; } = -1;
+        public int FinallyPointer { get; } = -1;
+        public int EndPointer { get; internal set; } = -1;
+        public bool HasCatch => CatchPointer >= 0;
+        public bool HasFinally => FinallyPointer >= 0;
         public ExceptionHandingState State { get; internal set; } = ExceptionHandingState.Try;
 
         public ExceptionHandingContext(ExecutionContext ExecutionContext, int catchOffset, int finallyOffset)
         {
             this.ExecutionContext = ExecutionContext;
-            this.HasCatch = catchOffset != 0;
-            this.HasFinally = finallyOffset != 0;
-            this.CatchPointer = checked(ExecutionContext.InstructionPointer + catchOffset);
-            this.FinallyPointer = checked(ExecutionContext.InstructionPointer + finallyOffset);
+            if (catchOffset != 0)
+                this.CatchPointer = checked(ExecutionContext.InstructionPointer + catchOffset);
+            if (finallyOffset != 0)
+                this.FinallyPointer = checked(ExecutionContext.InstructionPointer + finallyOffset);
         }
     }
 }
