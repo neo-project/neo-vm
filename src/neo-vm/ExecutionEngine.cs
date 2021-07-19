7fc0188dac90b41ee2922fdf0b59e63668350489
Jinghui Liao <jinghui@wayne.edu>
2021-07-19 15:35:50 +0800

    Update ExecutionEngine.cs

--- src/neo-vm/ExecutionEngine.cs
+++ src/neo-vm/ExecutionEngine.cs
@@ -1272,8 +1272,6 @@ namespace Neo.VM
                     {
                         VMArray x = Pop<VMArray>();
                         int index = x.Count - 1;
-                        if (index < 0)
-                            throw new InvalidOperationException($"The value {index} is out of range.");
                         Push(x[index]);
                         x.RemoveAt(index);
                         break;
