17838b833b2b4dbe82864954aeafdf4c8681cdc9
Jinghui Liao <jinghui@wayne.edu>
2021-08-19 14:31:08 +0800

    Add Copyright to NEOVM Files (#435)
    * add copyright to files
    
    * update copyright
    
    * update copyright
    
    * update copyright
    
    Co-authored-by: Erik Zhang <erik@neo.org>

--- src/neo-vm/GlobalSuppressions.cs
+++ src/neo-vm/GlobalSuppressions.cs
@@ -1,3 +1,13 @@
+// Copyright (C) 2016-2021 The Neo Project.
+// 
+// The neo-vm is free software distributed under the MIT software license, 
+// see the accompanying file LICENSE in the main directory of the
+// project or http://www.opensource.org/licenses/mit-license.php 
+// for more details.
+// 
+// Redistribution and use in source and binary forms with or without
+// modifications are permitted.
+
 // This file is used by Code Analysis to maintain SuppressMessage
 // attributes that are applied to this project.
 // Project-level suppressions either have no target or are given
