2fda3ed0b14cc71d27d00add790d8540c663fc13
Jinghui Liao <jinghui@wayne.edu>
2021-11-04 16:18:58 -0400

    update logic

--- src/neo-vm/ExecutionEngine.cs
+++ src/neo-vm/ExecutionEngine.cs
@@ -370,10 +370,12 @@ namespace Neo.VM
                 case OpCode.ASSERT:
                     {
                         var x = Pop().GetBoolean();
-                        string msg = "";
-                        if (CurrentContext.Compiler is not null
-                            && String.CompareOrdinal(CurrentContext.Compiler, "nccs v3.0.3") > 0)
-                            msg = $" error message: {Pop().GetString()}";
+                        string? msg = "";
+                        if (String.CompareOrdinal(CurrentContext.Compiler, "nccs v3.0.3") > 0)
+                        {
+                            msg = Pop().GetString();
+                            msg = msg is { Length: > 0 } ? $" error message: {msg}" : "";
+                        }
                         if (!x)
                             throw new Exception($"{OpCode.ASSERT} is executed with false result{msg}.");
                         break;
