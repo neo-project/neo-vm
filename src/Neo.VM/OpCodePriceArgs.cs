2ba50ee169efcbdedc36aa7e043dd83d19c38100
Tural Devrishev <tural@nspcc.ru>
2026-04-21 11:49:41 +0300

    add comments for OpcodePriceParams fields
    Signed-off-by: Tural Devrishev <tural@nspcc.ru>

--- src/Neo.VM/OpCodePriceArgs.cs
+++ src/Neo.VM/OpCodePriceArgs.cs
@@ -18,8 +18,26 @@ namespace Neo.VM;
 /// </summary>
 public struct OpcodePriceParams
 {
+    /// <summary>
+    /// Typ specifies type of <see cref="StackItemType"/> which in most of the cases serves
+	/// as an operand of the given opcode.
+    /// </summary>
     public StackItemType Type { get; internal set; }
+    /// <summary>
+    /// Length denotes one of the following:
+    /// - the number of elements in the compound type (that is, the length of Array
+    ///   or Struct, the number of key-value pairs in Map);
+    /// - the length of Buffer or ByteArray;
+    /// - the number of VM slot cells or stack elements involved in opcode handling;
+    /// - the number of stack elements that the opcode processes.
+    /// </summary>
     public int Length { get; internal set; }
+    /// <summary>
+    /// RefsDelta is total change of refCounter value performed by opcode.
+    /// </summary>
     public int RefsDelta { get; internal set; }
+    /// <summary>
+    /// NClonedItems is number of items cloned by opcode.
+    /// </summary>
     public int NClonedItems { get; internal set; }
 }
